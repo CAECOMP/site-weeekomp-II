e576c979b43d4231da1467af44b3567fb532b2fb
MarioMatheus <mariomatheu@gmail.com>
2019-09-11 20:37:16 -0300

    updating some things from rules

--- src/screens/rules/rules.js
+++ src/screens/rules/rules.js
@@ -21,7 +21,8 @@ class Rules extends Component{
             'Para confirmar sua inscrição é preciso levar no Centro Acadêmico(localização no Footer do site) pelo menos 2 Quilos de alimentos.','Assim sua inscrição será confirmada e você poderá garantir sua vaga.',
             'Caso a segunda fase da inscrição não seja feita, as inscrições nos eventos serão desconsiderados após o fim do prazo de entrega dos alimentos.']
         ,infoDip:['Só irão ganhar certificados aqueles que passarem pelas duas fases de inscrições e confirmar sua presença nos eventos de acordo com o credenciamento.']
-        ,infopart:['O Centro Acadêmico está em parceria com o instituto Céu no Sertão, que fará uma ação social durante o dia das crianças e além dos alimentos você pode levar:','- Mais alimentos','- Brinquedos','- Roupas','Para saber mais sobre o projeto e sobre a ação social que será realizada dia 12 de outubro clique abaixo:']
+        ,infopart:['O Centro Acadêmico está em parceria com o instituto Céu no Sertão, que fará uma ação social durante o dia das crianças e além dos alimentos você pode levar:','- Mais alimentos','- Brinquedos','- Roupas','Dia 12 de outubro ocorrerá o IMPACTO SERTÃO KIDS']
+        ,infoKids:['O que é o instituto?', 'O instituto céu no sertão existe a 4 anos na cidade de Sobral! Existe uma base de apoio na cidade, e trabalhos sendo desenvolvidos no sertão, numa comunidade chamada Boqueirão! Crianças tem assistência médica, famílias são beneficiadas com sextas básicas e muito amor!','O que irão fazer no dia das crianças ?', 'Nos Dias 11, 12 e 13 de outubro, estará acontecendo o impacto sertão kids, onde o instituto promoverá 3 dias de pura diversão e alegria para as crianças da comunidade, com jogos, brincadeiras, momentos de oração, e muita comida! Além disso, serão arrecadados e entregues as famílias das crianças, cestas básicas, roupas, brinquedos e etc !','Instagram:', <a href ="https://www.instagram.com/institutoceunosertao/">@institutoceunosertao</a>]
         ,infopay:['Os pagamentos deverão ser feitos por transferência bancária, pick-pay','PicPay: @jardelalves_bf','Nuconta:','->  Nome: Italo Bruno Cunha da Silva','->  CPF: 054.888.993-75','->  Banco: 260 - Nu Pagamentos S.A.','->  Agência: 0001','->  Conta: 8270524-0','Pagamento em espécie: Confira horários de atendimento do CAECOMP no rodapé do site']
        }
    }
@@ -33,6 +34,7 @@ class Rules extends Component{
                 <CardDefault title='Sobre as Inscrições'  description={this.state.infoSubs} /> 
                 <CardDefault title='Sobre os Certificados'  description={this.state.infoDip} />
                 <CardDefault title='Parceria '  description={this.state.infopart} />
+                <CardDefault title='Impacto Sertão Kids '  description={this.state.infoKids} />
                 <CardDefault title='Pagamentos '  description={this.state.infopay} />
                 <Footer/>
             </div>
