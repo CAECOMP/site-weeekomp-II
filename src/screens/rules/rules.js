020348f4ce72afca57c41e7d837ceacac835a252
MarioMatheus <mariomatheu@gmail.com>
2019-09-11 19:39:23 -0300

    updating some texts from rules

--- src/screens/rules/rules.js
+++ src/screens/rules/rules.js
@@ -19,10 +19,10 @@ class Rules extends Component{
            'Caso você entre no evento pela lista de espera, você entrará automaticamente e receberá um email de confirmação.',
             '2 - Confirmação presencial:',
             'Para confirmar sua inscrição é preciso levar no Centro Acadêmico(localização no Footer do site) pelo menos 2 Quilos de alimentos.','Assim sua inscrição será confirmada e você poderá garantir sua vaga.',
-            'Caso a segunda fase da inscrição não seja feita, as inscrições nos eventos serão desconsiderados após o encerrado o prazo de entrega.']
+            'Caso a segunda fase da inscrição não seja feita, as inscrições nos eventos serão desconsiderados após o fim do prazo de entrega dos alimentos.']
         ,infoDip:['Só irão ganhar certificados aqueles que passarem pelas duas fases de inscrições e confirmar sua presença nos eventos de acordo com o credenciamento.']
-        ,infopart:['O Centro Acadêmico está em parceria com o instituto Céu no Sertão, que fará uma ação social durante o dia das crianças e além dos alimentos você pode levar:','Mais alimentos','Brinquedos','Roupas','Para saber mais sobre o projeto e sobre a ação social que será realizada dia 12 de outubro clique abaixo:']
-        ,infopay:['Os pagamentos deverão ser feitos por transferência bancária, pick-pay','PicPay: @jardelalves_bf','Nuconta:','Nome: Italo Bruno Cunha da Silva','CPF: 054.888.993-75','Banco: 260 - Nu Pagamentos S.A.','Agência: 0001','Conta: 8270524-0','*Pagamento em espécie: Confira horários de atendimento do CAECOMP no rodapé do site']
+        ,infopart:['O Centro Acadêmico está em parceria com o instituto Céu no Sertão, que fará uma ação social durante o dia das crianças e além dos alimentos você pode levar:','- Mais alimentos','- Brinquedos','- Roupas','Para saber mais sobre o projeto e sobre a ação social que será realizada dia 12 de outubro clique abaixo:']
+        ,infopay:['Os pagamentos deverão ser feitos por transferência bancária, pick-pay','PicPay: @jardelalves_bf','Nuconta:','->  Nome: Italo Bruno Cunha da Silva','->  CPF: 054.888.993-75','->  Banco: 260 - Nu Pagamentos S.A.','->  Agência: 0001','->  Conta: 8270524-0','Pagamento em espécie: Confira horários de atendimento do CAECOMP no rodapé do site']
        }
    }
     render(){
@@ -31,7 +31,7 @@ class Rules extends Component{
                 <Navbar/>
                 <CardDefault title='Sobre a loja:'  description={this.state.InfoStore} /> 
                 <CardDefault title='Sobre as Inscrições'  description={this.state.infoSubs} /> 
-                <CardDefault title='Sobre os Diplomas'  description={this.state.infoDip} />
+                <CardDefault title='Sobre os Certificados'  description={this.state.infoDip} />
                 <CardDefault title='Parceria '  description={this.state.infopart} />
                 <CardDefault title='Pagamentos '  description={this.state.infopay} />
                 <Footer/>
