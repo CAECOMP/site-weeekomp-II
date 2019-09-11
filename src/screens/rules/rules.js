948588d706dfb2f05d5fe01f4b08d57dc1e2b430
José Yuri <yurilima.ce@gmail.com>
2019-09-11 17:18:10 -0300

    Criando a tela contendo as regras do evento

---
+++ src/screens/rules/rules.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react'
+import Navbar from '../../components/commons/navbar'
+import Footer from '../../components/commons/footer'
+
+
+class Rules extends Component{
+    render(){
+        return(
+            <div>
+                <Navbar/>
+                <Footer/>
+
+            </div>
+            
+        )
+    }
+}
+
+
+export default Rules
