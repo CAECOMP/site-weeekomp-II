fd370ef43230770e48186a4f4ba2dd2df8de96a9
MarioMatheus <mariomatheu@gmail.com>
2019-09-14 15:38:19 -0300

    add error handling to create account page

--- src/screens/CreateAccount/CreateAccount.js
+++ src/screens/CreateAccount/CreateAccount.js
@@ -7,66 +7,56 @@ import './CreateAccount.css'
 
 export default class CreateAccount extends React.Component{
 
-    state = {errorMessage: ''}
+    state = {
+        loginError: null
+    }
 
     constructor(props) {
         super(props)
+        this.handleSubmit = this.handleSubmit.bind(this)
         const userID = localStorage.getItem('userID')
         const authToken = localStorage.getItem('userToken')
         if (userID && authToken) window.open('/perfil', '_self')
     }
 
-    // function handleErrorMessage(message) {
-    //     switch(message){
-    //         case 'User already exists':
-    //             this.setState({errorMessage: 'Uma conta já existe para esse email'})
-    //             break;
-    //         default:
-    //             this.setState({errorMessage: ''})
-    //             break;
-    //     }
-    //     return
-    // }
+    handleSubmit(data) {
+        this.setState({ loginError: null })
+        api.post('/auth/signup', {name: data.name_person, email: data.email, password: data.password})
+        .then((response)=>{
+            const result = response.data.data
+            localStorage.setItem('userToken', result.token)
+            localStorage.setItem('userID', result.userinfo.user_id)
+            localStorage.setItem('userName', result.userinfo.name)
+            window.open('/perfil', '_self')
+        })
+        .catch(error => {
+            const message = error.response.status === 400 ? 'Email já cadastrado' : 'Erro no servidor, tente novamente mais tarde'
+            this.setState({ loginError: message })
+        })
+    }
 
-    render(){
-
+    render() {
         const schema = Yup.object().shape({
             name_person: Yup.string().required('Seu nome é obrigatório'),
             email: Yup.string().email().required('Seu email é obrigatório'),
             password: Yup.string().required('Uma senha é necessária'),
             confirm_password: Yup.string().oneOf([Yup.ref('password'), null], 'As senhas devem ser iguais')
-        });
-        
-        function handleSubmit(data) {
-            console.log('vamu lá')
-            api.post('/auth/signup', 
-            {name: data.name_person, email: data.email, password: data.password})
-            .then((response)=>{
-                const result = response.data.data
-                localStorage.setItem('userToken', result.token)
-                localStorage.setItem('userID', result.userinfo.user_id)
-                localStorage.setItem('userName', result.userinfo.name)
-                window.open('/perfil', '_self')
-            })
-            .catch((error)=>{
-                console.log(error.response.data.data)
-            })
-        }
+        })
 
         return (
             <div className="containerCreateAccount" style={{backgroundColor: '#461000'}}>
                 <img className="logoCreateAccount" src="logo.png" alt=""/>
                 {/* <p>{this.state.errorMessage}</p> */}
-                <Form className="formCreateAccount" schema={schema} onSubmit={handleSubmit}>
+                <Form className="formCreateAccount" schema={schema} onSubmit={this.handleSubmit}>
                     <Input className="formInput" name="name_person" type="text" placeholder="Nome completo"/>
                     <Input className="formInput" name="email" type="email" placeholder="Seu email"/>
                     <Input className="formInput" name="password" type="password" placeholder="Senha"/>
                     <Input className="formInput" name="confirm_password" type="password" placeholder="Confirme senha"/>
 
+                    <p>{this.state.loginError}</p>
                     <button className="submitBtn" type="submit">Cadastrar</button>
                 </Form>
             </div>
-            
         )
     }
 
