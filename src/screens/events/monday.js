547acf285ac0bd27c5fd8fcff8883a7c5f70d676
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-15 19:36:27 -0300

    Kingdom's-Day-Desmockado

--- src/screens/events/monday.js
+++ src/screens/events/monday.js
@@ -21,13 +21,29 @@ export default class Monday extends React.Component {
                         <div className="row">
                             <CardBig
                                 title="The Last Ones - Lucas Tavares, André Vieira, Gabriela Bezerra e Luiza Macedo"
-                                content="Alunos perto de se formar irão contar suas experiências durante o curso e suas expectativas para o dia depois da conquista do diploma."
+                                content={
+                                    <div>
+                                        &emsp;
+                                        <h6 className="amber-text text-accent-4">LOCAL: Auditório Iran Raupp.</h6>
+                                        <h6 className="amber-text text-accent-4">HORÁRIO: 13h30.</h6>
+                                        &emsp;
+                                        <p>Alunos perto de se formar irão contar suas experiências durante o curso e suas expectativas para o dia depois da conquista do diploma.</p>
+                                    </div>
+                                }
                                 imageSrc={require('../../talks/the-last-of-ones.png')}
                                 buttonLink="#" buttonName="Inscrever-se"
                             />
                             <CardBig
                                 title="Odin's Table - David, Alan Rabelo, Leo Silveira, Levi Moreira"
-                                content="Roda de conversa com ex-alunos que seguiram caminhos diferentes após a graduação. Falaremos com alunos de Mestrado, ex-alunos que abriram empresas ou foram para o mercado de trabalho. Eles irão tirar dúvidas e inspirar na hora de escolher o melhor caminho após a graduação."
+                                content={
+                                    <div>
+                                        &emsp;
+                                        <h6 className="amber-text text-accent-4">LOCAL: Auditório Iran Raupp.</h6>
+                                        <h6 className="amber-text text-accent-4">HORÁRIO: 16h.</h6>
+                                        &emsp;
+                                        <p>Roda de conversa com ex-alunos que seguiram caminhos diferentes após a graduação. Falaremos com alunos de Mestrado, ex-alunos que abriram empresas ou foram para o mercado de trabalho. Eles irão tirar dúvidas e inspirar na hora de escolher o melhor caminho após a graduação.</p>
+                                    </div>
+                                }
                                 imageSrc={require('../../talks/odins-table.png')}
                                 buttonLink="#" buttonName="Inscrever-se"
                             />
